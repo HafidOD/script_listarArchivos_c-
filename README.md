c78165824f51561a340072cf242f2d5fb8568e89
HAFID OXTE DZUL <nuevo@correo.com>
2020-05-25 19:28:20 -0500

    alpha

---
+++ README.md
@@ -0,0 +1,2 @@
+~~~
+g++ -o out ls-linux.cpp
---
+++ ls-linux.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <dirent.h>
+
+using namespace std;
+
+void list_dir(string dir)
+{
+	string PWD =  "ls -l "+ dir; // En windows string PWD =  "dir "+ dir;
+  //cout << PWD << endl;  
+  const char *command = PWD.c_str(); 
+  system(command);
+}
+
+void init()
+{
+	cout << "Escribe la ruta absoluta del directorio a listar: (C:/user/example): ";
+	string dir;
+	getline(cin, dir);
+	list_dir(dir);
+  cout << endl;
+	init();
+}
+
+int main(int argc, char *argv[])
+{
+	//system((char*)"pwd");
+  init();
+	return 0;
+}
